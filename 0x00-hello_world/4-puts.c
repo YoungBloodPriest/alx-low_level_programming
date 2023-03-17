ecf4bc4b593e9f6eee7070996bb8ae1e4e156d1e
YoungBloodPriest <hybridxlaw@gmail.com>
2023-03-17 07:23:38 -0700

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A programming code that prints a line with the put function
+ * Return: 0 (Successs)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
